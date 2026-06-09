64535e7be52238d11907e83d49d68a569f75acea
Moaz Elami <moazw8884@gmail.com>
2026-06-09 15:08:34 +0300

    problem

--- Sheet 4/J_Count_Letters.cpp
+++ Sheet 4/J_Count_Letters.cpp
@@ -5,11 +5,20 @@ int main()
 {
     string s;
     cin >> s;
-    int *freq = new int[s.length()];
-    for (size_t i = 0; i < s.length(); i++)
+    int *freq = new int[256];
+    for (int i = 0; i < 256; i++)
         freq[i] = 0;
 
+    for (int i = 0; i < s.length(); i++)
+    {
+        freq[s[i]]++;
+    }
 
+    for (char c = 'a'; c <= 'z'; c++)
+    {
+        if(freq[c] > 0)
+            cout << c << " : " << freq[c] << endl;
+    }
 
     return 0;
 }
